43621972123677207c92649d638178d20e7e8556
Bruno Rocha <rochab@spotify.com>
2020-10-09 13:07:44 +0200

    Add in place partial sorting

--- Sources/Algorithms/PartialSort.swift
+++ Sources/Algorithms/PartialSort.swift
@@ -1,89 +1,207 @@
-import Foundation
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Algorithms open source project
+//
+// Copyright (c) 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
 
-private final class Heap<T> {
+extension Sequence {
+    /// Returns the elements of the sequence such that the 0...k range contains
+    /// the first k sorted elements in this sequence, using the given predicate
+    /// as the comparison between elements.
+    ///
+    /// This example partially sorts an array of integers to retrieve its three
+    /// smallest values:
+    ///
+    ///     let numbers = [7,1,6,2,8,3,9]
+    ///     let almostSorted = numbers.partiallySorted(3, <)
+    ///     // [1, 2, 3, 9, 7, 6, 8]
+    ///     let smallestThree = almostSorted.prefix(3)
+    ///     // [1, 2, 3]
+    ///
+    /// The order of equal elements is not guaranteed to be preserved, and the
+    /// order of the remaining elements is unspecified.
+    ///
+    /// If you need to sort a sequence but only need access to a prefix of its elements,
+    /// using this method can give you a performance boost over sorting the entire
+    /// sequence.
+    ///
+    /// - Parameter count: The k number of elements to partially sort.
+    /// - Parameter areInIncreasingOrder: A predicate that returns true if its first argument should
+    /// be ordered before its second argument; otherwise, false.
+    ///
+    /// - Complexity: O(k log n)
+    public func partiallySorted(_ count: Int, by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows -> [Element] {
+      var result = ContiguousArray(self)
+      try result.partiallySort(count, by: areInIncreasingOrder)
+      return Array(result)
+    }
+}
 
-    typealias Comparator = (T,T) -> Bool
+extension Sequence where Element: Comparable {
+    /// Returns the elements of the sequence such that the 0...k range contains
+    /// the first k smallest elements in this sequence.
+    ///
+    /// This example partially sorts an array of integers to retrieve its three
+    /// smallest values:
+    ///
+    ///     let numbers = [7,1,6,2,8,3,9]
+    ///     let almostSorted = numbers.partiallySorted(3)
+    ///     // [1, 2, 3, 9, 7, 6, 8]
+    ///     let smallestThree = almostSorted.prefix(3)
+    ///     // [1, 2, 3]
+    ///
+    /// The order of equal elements is not guaranteed to be preserved, and the
+    /// order of the remaining elements is unspecified.
+    ///
+    /// If you need to sort a sequence but only need access to a prefix of its elements,
+    /// using this method can give you a performance boost over sorting the entire
+    /// sequence.
+    ///
+    /// - Parameter count: The k number of elements to partially sort, in ascending order.
+    ///
+    /// - Complexity: O(k log n)
+    public func partiallySorted(_ count: Int) -> [Element] {
+      return partiallySorted(count, by: <)
+    }
+}
 
-    private var elements: [T]
-    private let priority: Comparator
+extension MutableCollection where Self: RandomAccessCollection, Index == Int {
+    /// Rearranges this collection such that the 0...k range contains the first
+    /// k sorted elements in this collection, using the given predicate as the
+    /// comparison between elements.
+    ///
+    /// This example partially sorts an array of integers to retrieve its three
+    /// smallest values:
+    ///
+    ///     var numbers = [7,1,6,2,8,3,9]
+    ///     numbers.partiallySort(3, <)
+    ///     // [1, 2, 3, 9, 7, 6, 8]
+    ///     let smallestThree = numbers.prefix(3)
+    ///     // [1, 2, 3]
+    ///
+    /// The order of equal elements is not guaranteed to be preserved, and the
+    /// order of the remaining elements is unspecified.
+    ///
+    /// If you need to sort a collection but only need access to a prefix of its
+    /// elements, using this method can give you a performance boost over sorting
+    /// the entire collection.
+    ///
+    /// - Parameter count: The k number of elements to partially sort.
+    /// - Parameter areInIncreasingOrder: A predicate that returns true if its first argument should
+    /// be ordered before its second argument; otherwise, false.
+    ///
+    /// - Complexity: O(k log n)
+    public mutating func partiallySort(_ count: Int, by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows {
+      try __partiallySort(count, by: areInIncreasingOrder)
+    }
+}
 
-    init<S: Sequence>(elements: S, priority: @escaping Comparator) where S.Element == T {
-        self.priority = priority
-        self.elements = Array(elements)
-        if elements.isEmpty == false {
-            for i in stride(from: (count / 2) - 1, to: -1, by: -1) {
-                siftDown(i)
-            }
+extension MutableCollection where Self: RandomAccessCollection, Element: Comparable, Index == Int {
+    /// Rearranges this collection such that the 0...k range contains the first
+    /// k smallest elements in this collection.
+    ///
+    /// This example partially sorts an array of integers to retrieve its three
+    /// smallest values:
+    ///
+    ///     var numbers = [7,1,6,2,8,3,9]
+    ///     numbers.partiallySort(3)
+    ///     // [1, 2, 3, 9, 7, 6, 8]
+    ///     let smallestThree = numbers.prefix(3)
+    ///     // [1, 2, 3]
+    ///
+    /// The order of equal elements is not guaranteed to be preserved, and the
+    /// order of the remaining elements is unspecified.
+    ///
+    /// If you need to sort a collection but only need access to a prefix of its
+    /// elements, using this method can give you a performance boost over sorting
+    /// the entire collection.
+    ///
+    /// - Parameter count: The k number of elements to partially sort, in ascending order.
+    ///
+    /// - Complexity: O(k log n)
+    public mutating func partiallySort(_ count: Int) {
+      partiallySort(count, by: <)
+    }
+}
+
+//===----------------------------------------------------------------------===//
+// __partiallySort(_:by:)
+//===----------------------------------------------------------------------===//
+
+extension MutableCollection where Self: RandomAccessCollection, Index == Int {
+    typealias Priority = (Element, Element) throws -> Bool
+
+    /// Partially sorts this array by using an in place heapsort that stops after we find the desired k amount
+    /// of elements. The heap is stored and processed in reverse order so that the array doesn't have to be flipped
+    /// once the final result is found.
+    ///
+    /// Complexity: O(k log n)
+    mutating func __partiallySort(_ k: Int, by areInIncreasingOrder: Priority) rethrows {
+        assert(k >= 0, "Attempted to partially sort with a negative amount of elements!")
+        assert(k <= count,
+               "Attempted to partially sort an amount of elements larger than this Sequence's size!")
+        guard isEmpty == false else {
+            return
         }
+        var heapEndIndex = 0
+        for i in ((count / 2) + 1)..<count {
+            try siftDown(i, by: areInIncreasingOrder, heapEndIndex: heapEndIndex)
+        }
+        var iterator = (0..<k).makeIterator()
+        _ = iterator.next()
+        swapAt(count - 1, heapEndIndex)
+        heapEndIndex += 1
+        while let _ = iterator.next() {
+            try siftDown(count - 1, by: areInIncreasingOrder, heapEndIndex: heapEndIndex)
+            swapAt(count - 1, heapEndIndex)
+            heapEndIndex += 1
+        }
+    }
+
+    /// Sifts down an element from this heap.
+    /// The heap is stored in reverse order, so sifting down will actually move the element up in the heap array.
+    ///
+    /// - Parameter i: The element index to sift down
+    /// - Parameter by: The predicate to use when determining the priority of elements in the heap
+    /// - Parameter heapEndIndex: The index, in reverse order, where the heap ends.
+    private mutating func siftDown(_ i: Int, by priority: Priority, heapEndIndex: Int) rethrows {
+        let indexToSwap = try highestPriorityIndex(of: i, by: priority, heapEndIndex: heapEndIndex)
+        guard indexToSwap != i else {
+            return
+        }
+        swapAt(i, indexToSwap)
+        try siftDown(indexToSwap, by: priority, heapEndIndex: heapEndIndex)
+    }
+
+    private func highestPriorityIndex(of index: Int, by priority: Priority, heapEndIndex: Int) rethrows -> Int {
+        let reverseHeapTrueIndex = self.count - 1 - index
+        let leftChild = index - (leftChildIndex(of: reverseHeapTrueIndex) - reverseHeapTrueIndex)
+        let rightChild = index - (rightChildIndex(of: reverseHeapTrueIndex) - reverseHeapTrueIndex)
+        let left = try highestPriorityIndex(of: index, and: leftChild, by: priority, heapEndIndex: heapEndIndex)
+        let right = try highestPriorityIndex(of: index, and: rightChild, by: priority, heapEndIndex: heapEndIndex)
+        return try highestPriorityIndex(of: left, and: right, by: priority, heapEndIndex: heapEndIndex)
     }
 
     private func leftChildIndex(of index: Int) -> Int {
         return (2 * index) + 1
     }
 
-    private func rightChild(of index: Int) -> Int {
+    private func rightChildIndex(of index: Int) -> Int {
         return (2 * index) + 2
     }
 
-    private func parentIndex(of index: Int) -> Int {
-        return (index - 1) / 2
-    }
-
-    private func isHigherPriority(_ a: Int, _ b: Int) -> Bool {
-        return priority(elements[a], elements[b])
-    }
-
-    private func highestPriorityIndex(of index: Int) -> Int {
-        let left = highestPriorityIndex(of: index, and: leftChildIndex(of: index))
-        let right = highestPriorityIndex(of: index, and: rightChild(of: index))
-        return highestPriorityIndex(of: left, and: right)
-    }
-
-    private func highestPriorityIndex(of parent: Int, and child: Int) -> Int {
-        guard child < elements.count else {
+    private func highestPriorityIndex(of parent: Int, and child: Int, by priority: Priority, heapEndIndex: Int) rethrows -> Int {
+        guard child >= heapEndIndex else {
             return parent
         }
-        guard isHigherPriority(child, parent) else {
+        guard try priority(self[child], self[parent]) else {
             return parent
         }
         return child
-    }
-
-    func dequeue() -> T? {
-        guard elements.count > 0 else {
-            return nil
-        }
-        elements.swapAt(0, elements.count - 1)
-        let element = elements.popLast()
-        siftDown(0)
-        return element
-    }
-
-    private func siftDown(_ i: Int) {
-        let indexToSwap = highestPriorityIndex(of: i)
-        guard indexToSwap != i else {
-            return
-        }
-        elements.swapAt(indexToSwap, i)
-        siftDown(indexToSwap)
-    }
-}
-
-extension Collection {
-    func partiallySorted(_ count: Int, by: @escaping (Element, Element) -> Bool) -> [Element] {
-        assert(count >= 0 && count < self.count, "Are you crazy?")
-        let heap = Heap<Element>(elements: self, priority: by)
-        return [Element](unsafeUninitializedCapacity: count) { buffer, initializedCount in
-            for i in 0..<count {
-              buffer[i] = heap.dequeue()!
-            }
-            initializedCount = count
-        }
-    }
-}
-
-extension Collection where Element: Comparable {
-    func partiallySorted(_ count: Int) -> [Element] {
-      return partiallySorted(count, by: <)
     }
 }
