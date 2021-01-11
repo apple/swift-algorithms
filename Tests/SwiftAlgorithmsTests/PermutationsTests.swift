4416bab8ca356a1e0cf9296f80a34e693cce9b22
Matt Zanchelli <matt@mdznr.com>
2021-01-11 14:41:28 -0800

    Add tests for range-based `permutations(ofCount:)`

--- Tests/SwiftAlgorithmsTests/PermutationsTests.swift
+++ Tests/SwiftAlgorithmsTests/PermutationsTests.swift
@@ -26,27 +26,58 @@ final class PermutationsTests: XCTestCase {
       XCTAssertTrue(p.allSatisfy { $0.count == count })
       XCTAssertTrue(p.isSorted(by: { $0.lexicographicallyPrecedes($1) }))
     }
-
+    
+    func ts<R: RangeExpression>(count: R) where R.Bound == Int {
+      let p = count.relative(to: 0 ..< .max)
+        .clamped(to: 0 ..< c.count + 1)
+        .flatMap {
+          c.permutations(ofCount: $0)
+        }
+      
+      let p2 = c.permutations(ofCount: count)
+      
+      XCTAssertEqual(p.count, p2.count)
+      XCTAssertEqualSequences(p, Array(p2))
+    }
+    
+    t(count: 0)
     t(count: 1)
     t(count: 2)
     t(count: 3)
     t(count: 4)
     t(count: 5)
     t(count: nil)
+    
+    ts(count: 0...0)
+    ts(count: 1...1)
+    ts(count: 0...1)
+    ts(count: 0...2)
+    ts(count: 0...3)
+    ts(count: 1...3)
+    ts(count: 2...3)
+    ts(count: 0...)
+    ts(count: ...5)
+    ts(count: ...4)
+    ts(count: ...6)
   }
   
   func testEmpty() {
     // `k == 0` results in one zero-length permutation
     XCTAssertEqual(1, "".permutations().count)
     XCTAssertEqual(1, "ABCD".permutations(ofCount: 0).count)
+    XCTAssertEqual(1, "ABCD".permutations(ofCount: 0...0).count)
     XCTAssertEqual(Array("".permutations()), [[]])
     XCTAssertEqual(Array("".permutations(ofCount: 0)), [[]])
     XCTAssertEqual(Array("ABCD".permutations(ofCount: 0)), [[]])
+    XCTAssertEqual(Array("ABCD".permutations(ofCount: 0...0)), [[]])
     
     // `k` greater than element count results in zero permutations
     XCTAssertEqual(0, "".permutations(ofCount: 5).count)
     XCTAssertEqual(Array("".permutations(ofCount: 5)), [])
     XCTAssertEqual(Array("ABCD".permutations(ofCount: 5)), [])
+    XCTAssertEqual(Array("ABCD".permutations(ofCount: 5..<6)), [])
+    XCTAssertEqual(Array("ABCD".permutations(ofCount: 5..<7)), [])
+    XCTAssertEqual(Array("ABCD".permutations(ofCount: 5...)), [])
   }
   
   func testNextPermutation() {
@@ -64,7 +95,7 @@ final class PermutationsTests: XCTestCase {
     XCTAssertEqual([1, 2, 3, 4, 7, 6, 5], numbers)
     _ = numbers.nextPermutation()
     XCTAssertEqual([1, 2, 3, 5, 4, 6, 7], numbers)
-
+    
     // Fast-forward to end of permutations.
     while numbers.nextPermutation() {}
     XCTAssertEqual([1, 2, 3, 4, 5, 6, 7], numbers)
