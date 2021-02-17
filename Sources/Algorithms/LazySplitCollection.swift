8c32f20d8ae4070e7195bd2a5b4ac7b6115451bb
Todd Thomas <toddthomas@acm.org>
2021-02-17 13:51:18 -0700

    Add a lazy implementation of `split`.
    This implementation is provided only for `LazyCollection`-conforming types,
    because the strategy for avoiding new allocations is to have the iterator return
    subsequences via ranged subscripting of the underlying collection.

---
+++ Sources/Algorithms/LazySplitCollection.swift
@@ -0,0 +1,269 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Algorithms open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+/// A collection that lazily splits a collection into subsequences separated by elements that satisfy the given `whereSeparator` predicate.
+///
+/// - Note: This type is the result of `x.split(maxSplits:omittingEmptySubsequences:whereSeparator)` and
+/// `x.split(separator:maxSplits:omittingEmptySubsequences)`, where `x` conforms to `LazyCollection`.
+public struct LazySplitCollection<Base: LazyCollectionProtocol>: LazySequenceProtocol where Base.Element: Equatable, Base.Elements.Index == Base.Index {
+  internal let base: Base
+  internal let whereSeparator: (Base.Element) -> Bool
+  internal let maxSplits: Int
+  internal let omittingEmptySubsequences: Bool
+}
+
+extension LazySplitCollection {
+  public struct Iterator {
+    internal let base: Base
+    internal let whereSeparator: (Base.Element) -> Bool
+    internal let maxSplits: Int
+    internal let omittingEmptySubsequences: Bool
+    internal var subSequenceStart: Base.Index
+    internal var splitCount = 0
+
+    internal init(
+      base: Base,
+      whereSeparator: @escaping (Base.Element) -> Bool,
+      maxSplits: Int,
+      omittingEmptySubsequences: Bool
+    ) {
+      self.base = base
+      self.whereSeparator = whereSeparator
+      self.maxSplits = maxSplits
+      self.omittingEmptySubsequences = omittingEmptySubsequences
+      self.subSequenceStart = self.base.startIndex
+    }
+  }
+}
+
+extension LazySplitCollection.Iterator: IteratorProtocol {
+  public typealias Element = Base.Elements.SubSequence
+
+  public mutating func next() -> Element? {
+    guard subSequenceStart < base.endIndex else { return nil }
+
+    var subSequenceEnd: Base.Index
+    var lastAdjacentSeparator: Base.Index
+
+    if splitCount < maxSplits {
+      splitCount += 1
+      subSequenceEnd = base[subSequenceStart...].firstIndex(where: whereSeparator) ?? base.endIndex
+      lastAdjacentSeparator = subSequenceEnd
+
+      while omittingEmptySubsequences && lastAdjacentSeparator < base.endIndex {
+        let next = base.index(after: lastAdjacentSeparator)
+        if next < base.endIndex && whereSeparator(base[next]) {
+          lastAdjacentSeparator = next
+        } else {
+          break
+        }
+      }
+    } else {
+      subSequenceEnd = base.endIndex
+      lastAdjacentSeparator = subSequenceEnd
+    }
+
+    defer {
+      if lastAdjacentSeparator < base.endIndex {
+        subSequenceStart = base.index(after: lastAdjacentSeparator)
+      } else {
+        subSequenceStart = base.endIndex
+      }
+    }
+
+    return base.elements[subSequenceStart..<subSequenceEnd]
+  }
+}
+
+extension LazySplitCollection: Sequence {
+  public func makeIterator() -> Iterator {
+    return Iterator(
+      base: self.base,
+      whereSeparator: self.whereSeparator,
+      maxSplits: self.maxSplits,
+      omittingEmptySubsequences: self.omittingEmptySubsequences
+    )
+  }
+}
+
+extension LazyCollection where Element: Equatable {
+  /// Lazily returns the longest possible subsequences of the collection, in order,
+  /// that don't contain elements satisfying the given predicate.
+  ///
+  /// The resulting lazy sequence consists of at most `maxSplits + 1` subsequences.
+  /// Elements that are used to split the sequence are not returned as part of
+  /// any subsequence.
+  ///
+  /// The following examples show the effects of the `maxSplits` and
+  /// `omittingEmptySubsequences` parameters when lazily splitting a string using a
+  /// closure that matches spaces. The first use of `split` returns each word
+  /// that was originally separated by one or more spaces.
+  ///
+  ///     let line = "BLANCHE:   I don't want realism. I want magic!"
+  ///     for spaceless in line.lazy.split(whereSeparator: { $0 == " " }) {
+  ///       print(spaceless)
+  ///     }
+  ///     // Prints
+  ///     // BLANCHE:
+  ///     // I
+  ///     // don't
+  ///     // want
+  ///     // realism.
+  ///     // I
+  ///     // want
+  ///     // magic!
+  ///
+  /// The second example passes `1` for the `maxSplits` parameter, so the
+  /// original string is split just once, into two new strings.
+  ///
+  ///     for spaceless in line.lazy.split(maxSplits: 1, whereSeparator: { $0 == " " }) {
+  ///       print(spaceless)
+  ///     }
+  ///     // Prints
+  ///     // BLANCHE:
+  ///     // I don't want realism. I want magic!
+  ///
+  /// The final example passes `false` for the `omittingEmptySubsequences`
+  /// parameter, so the returned array contains empty strings where spaces
+  /// were repeated.
+  ///
+  ///     for spaceless in line.lazy.split(omittingEmptySubsequences: false, whereSeparator: { $0 == " " }) {
+  ///       print(spaceless)
+  ///     }
+  ///     // Prints
+  ///     // BLANCHE:
+  ///     //
+  ///     //
+  ///     // I
+  ///     // don't
+  ///     // want
+  ///     // realism.
+  ///     // I
+  ///     // want
+  ///     // magic!
+  ///
+  /// - Parameters:
+  ///   - maxSplits: The maximum number of times to split the collection, or
+  ///     one less than the number of subsequences to return. If
+  ///     `maxSplits + 1` subsequences are returned, the last one is a suffix
+  ///     of the original collection containing the remaining elements.
+  ///     `maxSplits` must be greater than or equal to zero. The default value
+  ///     is `Int.max`.
+  ///   - omittingEmptySubsequences: If `false`, an empty subsequence is
+  ///     returned in the result for each pair of consecutive elements
+  ///     satisfying the `isSeparator` predicate and for each element at the
+  ///     start or end of the collection satisfying the `isSeparator`
+  ///     predicate. The default value is `true`.
+  ///   - whereSeparator: A closure that takes an element as an argument and
+  ///     returns a Boolean value indicating whether the collection should be
+  ///     split at that element.
+  /// - Returns: A lazy sequence of subsequences, split from this collection's
+  ///   elements.
+  ///
+  /// - Complexity: O(*n*), where *n* is the length of the collection.
+  func split(
+    maxSplits: Int = Int.max,
+    omittingEmptySubsequences: Bool = true,
+    whereSeparator: @escaping (Base.Element) -> Bool
+  ) -> LazySplitCollection<Self> {
+    LazySplitCollection(
+      base: self,
+      whereSeparator: whereSeparator,
+      maxSplits: maxSplits,
+      omittingEmptySubsequences: omittingEmptySubsequences
+    )
+  }
+
+  /// Lazily returns the longest possible subsequences of the collection, in order,
+  /// around elements equal to the given element.
+  ///
+  /// The resulting lazy sequence consists of at most `maxSplits + 1` subsequences.
+  /// Elements that are used to split the collection are not returned as part
+  /// of any subsequence.
+  ///
+  /// The following examples show the effects of the `maxSplits` and
+  /// `omittingEmptySubsequences` parameters when splitting a string at each
+  /// space character (" "). The first use of `split` returns each word that
+  /// was originally separated by one or more spaces.
+  ///
+  ///     let line = "BLANCHE:   I don't want realism. I want magic!"
+  ///     for spaceless in line.lazy.split(separator: " ") {
+  ///       print(spaceless)
+  ///     }
+  ///     // Prints
+  ///     // BLANCHE:
+  ///     // I
+  ///     // don't
+  ///     // want
+  ///     // realism.
+  ///     // I
+  ///     // want
+  ///     // magic!
+  ///
+  /// The second example passes `1` for the `maxSplits` parameter, so the
+  /// original string is split just once, into two new strings.
+  ///
+  ///     for spaceless in line.lazy.split(separator: " ", maxSplits: 1) {
+  ///       print(spaceless)
+  ///     }
+  ///     // Prints
+  ///     // BLANCHE:
+  ///     // I don't want realism. I want magic!
+  ///
+  /// The final example passes `false` for the `omittingEmptySubsequences`
+  /// parameter, so the returned array contains empty strings where spaces
+  /// were repeated.
+  ///
+  ///     for spaceless in line.lazy.split(separator: " ", omittingEmptySubsequences: false) {
+  ///       print(spaceless)
+  ///     }
+  ///     // Prints
+  ///     // BLANCHE:
+  ///     //
+  ///     //
+  ///     // I
+  ///     // don't
+  ///     // want
+  ///     // realism.
+  ///     // I
+  ///     // want
+  ///     // magic!
+  ///
+  /// - Parameters:
+  ///   - separator: The element that should be split upon.
+  ///   - maxSplits: The maximum number of times to split the collection, or
+  ///     one less than the number of subsequences to return. If
+  ///     `maxSplits + 1` subsequences are returned, the last one is a suffix
+  ///     of the original collection containing the remaining elements.
+  ///     `maxSplits` must be greater than or equal to zero. The default value
+  ///     is `Int.max`.
+  ///   - omittingEmptySubsequences: If `false`, an empty subsequence is
+  ///     returned in the result for each consecutive pair of `separator`
+  ///     elements in the collection and for each instance of `separator` at
+  ///     the start or end of the collection. If `true`, only nonempty
+  ///     subsequences are returned. The default value is `true`.
+  /// - Returns: A lazy sequence of subsequences, split from this collection's
+  ///   elements.
+  ///
+  /// - Complexity: O(*n*), where *n* is the length of the collection.
+  func split(
+    separator: Element,
+    maxSplits: Int = Int.max,
+    omittingEmptySubsequences: Bool = true
+  ) -> LazySplitCollection<Self> {
+    LazySplitCollection(
+      base: self,
+      whereSeparator: { $0 == separator },
+      maxSplits: maxSplits,
+      omittingEmptySubsequences: omittingEmptySubsequences
+    )
+  }
+}
---
+++ Tests/SwiftAlgorithmsTests/LazySplitCollectionTests.swift
@@ -0,0 +1,87 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Algorithms open source project
+//
+// Copyright (c) 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import Algorithms
+
+final class LazySplitCollectionTests: XCTestCase {
+  func testInts() {
+    let nums = [1, 2, 42, 3, 4, 42, 5, 6, 42, 7]
+    let expectedResult = nums.split(separator: 42)
+    let testResult = nums.lazy.split(separator: 42)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testIntsWithMultipleAdjacentSeparators() {
+    let nums = [1, 2, 42, 3, 4, 42, 42, 5, 6, 42, 7]
+    let expectedResult = nums.split(separator: 42)
+    let testResult = nums.lazy.split(separator: 42)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testString() {
+    let path = "archive.tar.gz"
+    let expectedResult = path.split(separator: ".")
+    let testResult = path.lazy.split(separator: ".")
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testStringWithMultipleAdjacentSeparators() {
+    let line = "BLANCHE:   I don't want realism. I want magic!"
+    let expectedResult = line.split(separator: " ")
+    let testResult = line.lazy.split(separator: " ")
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testIntsWithMaxSplits() {
+    let nums = [1, 2, 42, 3, 4, 42, 5, 6, 42, 7]
+    let expectedResult = nums.split(separator: 42, maxSplits: 2)
+    let testResult = nums.lazy.split(separator: 42, maxSplits: 2)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testStringWithMaxSplits() {
+    let path = "archive.tar.gz"
+    let expectedResult = path.split(separator: ".", maxSplits: 1)
+    let testResult = path.lazy.split(separator: ".", maxSplits: 1)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testIntsWithoutOmittingEmptySubsequences() {
+    let nums = [1, 2, 42, 3, 4, 42, 42, 5, 6, 42, 7]
+    let expectedResult = nums.split(separator: 42, omittingEmptySubsequences: false)
+    let testResult = nums.lazy.split(separator: 42, omittingEmptySubsequences: false)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testStringWithoutOmittingEmptySubsequences() {
+    let line = "BLANCHE:   I don't want realism. I want magic!"
+    let expectedResult = line.split(separator: " ", omittingEmptySubsequences: false)
+    let testResult = line.lazy.split(separator: " ", omittingEmptySubsequences: false)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testIntsWithWhereClosure() {
+    let nums = [1, 2, 3, 4, 5, 6, 7]
+    let isEven = { $0 % 2 == 0 }
+    let expectedResult = nums.split(whereSeparator: isEven)
+    let testResult = nums.lazy.split(whereSeparator: isEven)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+
+  func testStringWithWhereClosure() {
+    let line = "BLANCHE:   I don't want realism. I want magic!"
+    let isSpace = { $0 == Character(" ") }
+    let expectedResult = line.split(whereSeparator: isSpace)
+    let testResult = line.lazy.split(whereSeparator: isSpace)
+    XCTAssertEqualSequences(testResult, expectedResult)
+  }
+}
