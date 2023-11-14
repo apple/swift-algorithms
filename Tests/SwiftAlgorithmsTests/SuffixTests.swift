5c54f200e7f559aadf961b1fc3910706d66b17a1
Nate Cook <natecook@apple.com>
2023-11-14 11:51:57 -0600

    Update guide/docs/tests.

--- Tests/SwiftAlgorithmsTests/SuffixTests.swift
+++ Tests/SwiftAlgorithmsTests/SuffixTests.swift
@@ -10,7 +10,7 @@
 //===----------------------------------------------------------------------===//
 
 import XCTest
-@testable import Algorithms
+import Algorithms
 
 final class SuffixTests: XCTestCase {
   func testSuffix() {
