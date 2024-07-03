7b29c5556b1fa92c2871d4ad36cca9e497f84b75
wangzhizhou <wangzhizhou@bytedance.com>
2024-07-03 12:39:02 +0800

    remove swiftdoccplugin

--- Package.swift
+++ Package.swift
@@ -21,7 +21,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
